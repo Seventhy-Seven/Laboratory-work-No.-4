1717fd80af31cdd08476853a1224d942c23fe462
Seventhy-Seven <72259094+Seventhy-Seven@users.noreply.github.com>
2020-10-08 21:22:16 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String [][]ar = {{" "," ", "/","|"},{" ", "/", " ", "|"},{"/", "_", "_", "|"}};
+        System.out.println();
+        System.out.println(ar[0][0] + ar[0][1] + ar [0][2] + ar [0][3]);
+        System.out.println(ar[1][0] + ar [1][1] + ar [1][2] + ar [1][3]);
+        System.out.println(ar[2][0] + ar[2][1] + ar[2][2] + ar[2][3]);
+    }
+}
